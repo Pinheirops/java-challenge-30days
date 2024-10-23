c5f808f98776ebbe1e10a1e359390cce19b343b3
Pinheirops <ppinheirouser@gmail.com>
2024-10-23 14:36:05 -0300

    refatoração e correção de funções

--- src/calculadora/view/CalculadoraApp.java
+++ src/calculadora/view/CalculadoraApp.java
@@ -6,33 +6,57 @@ public class CalculadoraApp {
     public static void main(String[] args){
 
         Scanner teclado = new Scanner(System.in);
+        double acumulador = 0;
 
-        System.out.println("OPERAÇÕES");
-        System.out.println("+ - Soma");
-        System.out.println("- - Subtração");
-        System.out.println("* - Multiplicação");
-        System.out.println("/ - Divisão");
-        System.out.println("C - Zerar");
+        System.out.println("--------------------");
+        System.out.println("| " + acumulador +"              |");
+        System.out.println("|------------------|");
+        System.out.println("| OPERAÇÕES:       |");
+        System.out.println("| ( + ) Soma       |");
+        System.out.println("| ( - ) Subtração  |");
+        System.out.println("| ( * ) - Multip   |");
+        System.out.println("| ( / )- Divisão   |");
+        System.out.println("| ( C )- Zerar     |");
+        System.out.println("|------------------|");
 
-        System.out.println("Entre com um número");
-        double numero = teclado.nextInt();
-        double acumulador = numero;
+        System.out.print("| "  );
+        double numero = teclado.nextDouble();
+        acumulador = acumulador + numero;
 
-        System.out.println("DISPLAY: " + acumulador);
-
-        System.out.println("Digite o sinal da operação desejada");
+        System.out.print("| " + acumulador + " ");
         String opSelect = teclado.next();
 
         if(opSelect.equals("+")){
             while(opSelect.equals("+")){
-                System.out.print(acumulador + " + ");
-                numero = teclado.nextInt();
+                System.out.print("| " + acumulador + " + ");
+                numero = teclado.nextDouble();
+
+                // Essa parte ainda não está funcionando conforme o esperado.
                 somar(numero, acumulador);
                 acumulador = acumulador + numero;
-                System.out.println("DISPLAY: " + acumulador);
+
+                System.out.println("RESULTADO: " + acumulador);
+                System.out.println("-");
+
                 System.out.println("Digite o sinal da operação desejada");
                 opSelect = teclado.next();
             }
+        }
+
+        if(opSelect.equals("-")){
+            System.out.print("| " + acumulador + " - ");
+            numero = teclado.nextDouble();
+
+            // Essa parte ainda não está funcionando conforme o esperado.
+            somar(numero, acumulador);
+            acumulador = acumulador - numero;
+
+            System.out.println("RESULTADO: " + acumulador);
+            System.out.println("-");
+
+
+            System.out.println("Digite o sinal da operação desejada");
+            opSelect = teclado.next();
         }
 
 
@@ -44,5 +68,20 @@ public class CalculadoraApp {
     private static double somar(double numero, double valorTotal) {
         double soma = numero + valorTotal;
         return soma;
+    }
+
+    private static double subtrair(double numero, double valorTotal){
+        double subtracao = numero - valorTotal;
+        return subtracao;
+    }
+
+    private static double multiplicar(double numero, double valorTotal){
+        double multiplicacao = numero * valorTotal;
+        return multiplicacao;
+    }
+
+    private static double dividir(double numero, double valorTotal){
+        double divisao = numero / valorTotal;
+        return divisao;
     }
 }
